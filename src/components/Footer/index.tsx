0199448b520f6d9d4763fcd448cb8d7a805b4448
jdmiguel <jaimedmiguel@gmail.com>
2020-07-29 08:54:02 +0100

    add component structure and helpers folder

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer:React.FC = () => {
+  return(
+    <p>Footer</p>
+  )
+}
+
+export default Footer;
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main:React.FC = () => {
+  return(
+    <p>Main</p>
+  )
+}
+
+export default Main;
