75610846259c0f0064ccc25bf3cb631a22a08ee6
jdmiguel <jaimedmiguel@gmail.com>
2020-07-29 11:03:24 +0100

    style app and header, add hide / show functionality on click header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useReducer, Dispatch } from 'react';
+
+import Header from '../Header';
+
+type ClassesAction = { type: 'hide'} | { type: 'show' };
+
+const classesReducer = (classes: string[], action: ClassesAction) => {
+  switch (action.type) {
+    case 'hide':
+      return [...classes,'hide'];
+    case 'show':
+      return classes.filter(appClass => appClass !== 'hide');
+  }
+};
 
 const App:React.FC = () => {
+  const [appClasses, dispatch]: [
+    string[],
+    Dispatch<ClassesAction>,
+  ] = useReducer(classesReducer, ['app']);
+
+
+  const handleAppClasses = () => {
+    appClasses.includes('hide') ? dispatch({type: 'show'}) : dispatch({type: 'hide'})
+  }
+
   return(
-    <p>APP</p>
+    <div className={appClasses.join(' ')}>
+      <Header onClick={handleAppClasses}/>
+    </div>
   )
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
-const Header:React.FC = () => {
+export type HeaderProps = {
+  onClick: () => void;
+};
+
+const Header:React.FC<HeaderProps> = ({onClick}) => {
   return(
-    <p>Header</p>
+    <header>
+      <button type="button" onClick={onClick}></button>
+      <div className="header-profile">
+        <i data-testid={'label-icon'} className="material-icons">
+          account_circle
+        </i>
+        <div>
+          <h1>Ipresence User</h1>
+          <h2>Online</h2>
+        </div>
+      </div>
+      <div>
+        <h3>150 new messages</h3>
+      </div>
+    </header>
   )
 }
 
